b9f46691c97f012a1e9c7f31d4573c956c374608
cenourinha542 <163136505+cenourinha542@users.noreply.github.com>
2024-03-12 09:39:39 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function mudarimagemfora()
+{
+    document.getElementById("imagem").src ="https://mixdeseries.com.br/wp-content/uploads/2023/07/cena-da-serie-the-chosen.webp";
+}
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src ="https://cdnph.upi.com/ph/st/th/2711689592062/2023/i/16895941201825/v1.2/The-Chosen-granted-waiver-to-continue-filming-through-SAG-strike.jpg?lg=5&=1";
+}
